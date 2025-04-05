3ca7ea1a6d2b7d30cffe3201d57fe887abb5fe0f
Joseph <liujingtao529@gmail.com>
2025-04-06 00:41:47 +1100

    improve parent and children maven management

---
+++ microshop-products/src/main/java/com/codeandcry/microshop/products/AttrRepository.java
@@ -0,0 +1,8 @@
+package com.codeandcry.microshop.products;
+
+import com.codeandcry.microshop.products.models.Attr;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AttrRepository extends JpaRepository<Attr, Long> {
+
+}
--- microshop-products/src/test/java/com/codeandcry/microshop/products/MicroshopProductsApplicationTests.java
+++ microshop-products/src/test/java/com/codeandcry/microshop/products/MicroshopProductsApplicationTests.java
@@ -1,13 +1,18 @@
 package com.codeandcry.microshop.products;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MicroshopProductsApplicationTests {
 
+    @Autowired
+    private AttrRepository attrRepository;
+
     @Test
     void contextLoads() {
+        attrRepository.findAll().forEach(System.out::println);
     }
 
 }
